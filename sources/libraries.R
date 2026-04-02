681927bd52638eb8e04695561b36d189ee895b82
Martin Hennessy MBA-M2 <martinhennessy@pm.me>
2026-04-02 11:28:06 +0100

    new libraries for survival

--- sources/libraries.R
+++ sources/libraries.R
@@ -3,3 +3,5 @@ library(readxl)      # reading .xlsx data files
 library(lubridate)   # working with dates
 library(gt)          # formatted tables
 library(gtsummary)   # publication-ready summary/Table 1 tables
+library(survival)    # Surv() and survfit() for Kaplan-Meier analysis
+library(survminer)   # ggsurvplot() for publication-ready survival curves
